6a04fa8e9992fd5a7b86498b18574557e8cfae91
wanbin-dev <eamil@wanbin.tech>
2021-11-30 18:38:30 +0800

    update

--- wifi_test/wifi_test.ino
+++ wifi_test/wifi_test.ino
@@ -1,44 +1,44 @@
 #include "Stepper.h"
 #include "ClientSocket.h"
 
-//Stepper *stepper = NULL;
-//ClientSocket *socket;
+Stepper *stepper = NULL;
+ClientSocket *socket = NULL;
 
 void setup()
 {
 	Serial.begin(9600);
 
-	//*socket = newClientSocket();
-	//socket->ConnectWiFi();
+	socket = newClientSocket();
+	socket->ConnectWiFi();
 
-	// *stepper = Stepper(D0, D1, D2, D3, 3);
+	stepper = new Stepper(D0, D1, D2, D3, 3);
 }
 
 void loop()
 {
-	// String signal = socket->ReadContent('\n');
-	// if (signal == "")
-	// {
-	// 	return;
-	// }
+	String signal = socket->ReadContent('\n');
+	if (signal == "")
+	{
+		return;
+	}
 
-	// if (signal == "open")
-	// {
-	// 	openDoor();
-	// }
+	if (signal == "open")
+	{
+		openDoor();
+	}
 }
 
-// ClientSocket newClientSocket()
-// {
-// 	SocketConfig config;
-// 	config.SSID = "WA";
-// 	config.Password = "wivia1868";
-// 	config.StaticIP = IPAddress(192, 168, 3, 200);
-// 	config.Dns = IPAddress(255, 255, 255, 0);
-// 	config.Gateway = IPAddress(192, 168, 3, 1);
-// 	config.Server = IPAddress(192, 168, 3, 93);
-// 	return ClientSocket(config);
-// }
+ClientSocket *newClientSocket()
+{
+	SocketConfig config;
+	config.SSID = "WA";
+	config.Password = "wivia1868";
+	config.StaticIP = IPAddress(192, 168, 3, 200);
+	config.Dns = IPAddress(255, 255, 255, 0);
+	config.Gateway = IPAddress(192, 168, 3, 1);
+	config.Server = IPAddress(192, 168, 3, 93);
+	return new ClientSocket(config);
+}
 
 void openDoor()
 {
