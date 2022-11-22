994a3ba3a56c1b2b82111efdd550421663171520
binwan-dev <email@wanbin.tech>
2022-11-22 15:19:36 +0800

    update wxserver

--- wxserver/Controllers/LockerController.cs
+++ wxserver/Controllers/LockerController.cs
@@ -10,7 +10,7 @@ namespace BinHome.WxServer.Controllers
         {
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://8.217.197.89:8003/");
+                client.BaseAddress = new Uri("https://locker.wanbin.tech");
                 var resp = await client.PostAsync("/locker/opendoor", new StringContent(""));
                 HttpContext.Response.StatusCode = (int)resp.StatusCode;
                 HttpContext.Response.Body = await resp.Content.ReadAsStreamAsync();
